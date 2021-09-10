dcd6c342ac0687dbe1aa0a7ecd4f00948568544a
Instant Coffee <load1999@gmail.com>
2021-09-10 12:29:37 +0900

    9.10

--- Algorithm/Programmers/LV1/다트게임.js
+++ Algorithm/Programmers/LV1/다트게임.js
@@ -1,48 +1,46 @@
 function solution(dartResult) {
     let answer = 0;
     let temp = ''
-    let arr = []
+    let memo = []
     let starCount = 0
     for(let i=0; i<dartResult.length; i++){
         if(typeof +dartResult[i] === 'number' && isNaN(dartResult[i])===false){
-            if(dartResult[i] === '1' && dartResult[i+1] === '0'){
-                temp = 10
-            } else if(dartResult[i] === '0' && dartResult[i-1]==='1'){
-                continue
-        }
-
-            else {
-                temp = dartResult[i]
+            if(dartResult[i] === '1' && dartResult[i+1] === '0'){ // 10일때 조건 : 1일때 뒷자리가 0인지 확인
+                temp = 10 // 직접 10 넣고 
+            } else if(dartResult[i] === '0' && dartResult[i-1]==='1'){ // 0이나오고, 뒷자리가 1일때 1:[i-1] 0:[i] 
+                continue // 아무것도 안하고 넘김
+        } else {
+                temp = dartResult[i] // 나머지경우(0~9)는 temp에 넣는다.
             }
         }
-        if(dartResult[i] === 'S'){
-            temp = Math.pow(temp,1)
-            arr.push(temp)
-            answer += temp;
+        if(dartResult[i] === 'S'){ // "S"일경우
+            temp = Math.pow(temp,1) // 1제곱
+            memo.push(temp) // 배열에 memo해준다
+            answer += temp; // answer에 더한다.
         }
-        if(dartResult[i] === 'D'){
-            temp = Math.pow(temp,2)
-            arr.push(temp)
-            answer += temp;
+        if(dartResult[i] === 'D'){ // "D"일경우
+            temp = Math.pow(temp,2) // 2제곱
+            memo.push(temp) // 배열에 memo한다.
+            answer += temp; //answer에 더한다
         }
-        if(dartResult[i] === 'T'){
-            temp = Math.pow(temp,3)
-            arr.push(temp)
-            answer += temp;
+        if(dartResult[i] === 'T'){ //"T"일경우
+            temp = Math.pow(temp,3) // 3제곱
+            memo.push(temp) // 배열에 memo
+            answer += temp; // answer에 더한다.
         }
-        if(dartResult[i] === "#"){
-            answer = answer - temp*2
+        if(dartResult[i] === "#"){ // "#"인경우
+            answer = answer - temp*2 // 로직상 기존에 더했던거까지 빼주려면 "곱하기 2"해줘야함
         }
         if(dartResult[i] === "*"){ // star의 갯수가 여러개 나올 수 있음. star가 뒤에 나오면 두번째 세번째만 곱해준다.
-            if(i>dartResult.length-3 && starCount === 2){
-                answer = answer*2 -(arr[0] * Math.pow(2,starCount)) // 기존에 별만큼 뺀 값을 더해준거 모두 제거
+            if(i>dartResult.length-3 && starCount === 2){ // *** star가 첫번째나 두번째가 아닌경우만 다른 로직 ***
+                answer = answer*2 -(memo[0] * Math.pow(2,starCount)) // 기존에 별만큼 뺀 값을 더해준거 모두 제거
             }
             else if(i>dartResult.length-3 && starCount === 1){
-                answer = answer*2 -(arr[0] * Math.pow(2,starCount)) // 기존에 별만큼 뺀 값을 더해준거 모두 제거
+                answer = answer*2 -(memo[0] * Math.pow(2,starCount)) // 기존에 별만큼 뺀 값을 더해준거 모두 제거
                 starCount++
             }
             else if(i>dartResult.length-3 && starCount === 0){
-                answer = answer*2 - (arr[0] * Math.pow(2,starCount)) // 기존에 별만큼 뺀 값을 더해준거 모두 제거
+                answer = answer*2 - (memo[0] * Math.pow(2,starCount)) // 기존에 별만큼 뺀 값을 더해준거 모두 제거
                 starCount++
         } else {
                 answer = answer * 2
