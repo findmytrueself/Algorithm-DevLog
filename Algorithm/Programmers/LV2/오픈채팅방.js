4d1c7b7b318f025a7ba00b66ff0e300a3543d959
Instant Coffee <load1999@gmail.com>
2021-09-21 23:47:57 +0900

    9.21

---
+++ Algorithm/Programmers/LV2/오픈채팅방.js
@@ -0,0 +1,61 @@
+function solution(record) {
+    var answer = [];
+    let keyword = '';
+    let id = '';
+    let name = '';
+    let userLog = [];
+    record.map((el) => {
+        keyword = el.split(' ')[0];
+        id = el.split(' ')[1];
+        name = el.split(' ')[2];
+        if (keyword === 'Enter' && !userLog.flat().includes(id)) {
+            answer.push(`${name}님이 들어왔습니다.`);
+            userLog.push([id, name]);
+        } else if (keyword === 'Enter' && userLog.flat().includes(id)) {
+            let tempName = '';
+            answer.push(`${name}님이 들어왔습니다.`);
+            userLog.push([id, name]);
+            userLog.some((ele) => {
+                if (ele[0] === id && ele[1] !== name) {
+                    tempName = ele[1];
+                }
+            });
+            answer = answer.map((ele) => ele.replace(tempName, name));
+        }
+        if (keyword === 'Leave') {
+            userLog.map((ele) => {
+                if (ele[0] === id) {
+                    name = ele[1];
+                    answer.push(`${name}님이 나갔습니다.`);
+                    userLog.push([id, name]);
+                    if (ele[2] === 1) {
+                        answer.splice(-2, 1);
+                        userLog.splice(-2, 1);
+                    }
+                }
+            });
+        }
+        if (keyword === 'Change') {
+            let tempName = '';
+            userLog.some((ele) => {
+                if (ele[0] === id) {
+                    tempName = ele[1];
+                }
+            });
+            answer = answer.map((ele, idx) => {
+                if (userLog[idx][0] === id) {
+                    userLog.push([id, name, 1]);
+                    return ele.replace(tempName, name);
+                } else {
+                    return ele;
+                }
+            });
+        }
+    });
+    console.log(answer);
+    return answer;
+}
+debugger;
+// solution(['Enter uid1234 Muzi', 'Enter uid4567 Prodo', 'Leave uid1234', 'Enter uid1234 Prodo', 'Change uid4567 Ryan']);
+solution(['Enter uid123 uid1234', 'Leave uid123', 'Enter uid4567 uid123', 'Change uid4567 uid1234', 'Leave uid4567']);
+// ["uid1234님이 들어왔습니다.","uid1234;님이 나갔습니다.","uid1234님이 들어왔습니다.","uid1234님이 나갔습니다."]
